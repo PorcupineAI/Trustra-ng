932d2fd434dffcef5be3b884bddfb23094894b91
PorcupineAI <okerekevip@gmail.com>
2025-12-30 17:14:01 +0100

    Refactor trust score calculation to update method

--- backend/app/intelligence/trust_score.py
+++ backend/app/intelligence/trust_score.py
@@ -1,13 +1,7 @@
-def calculate_trust_score(user):
-    score = 50  # neutral base
+def update_trust_score(user, fraud_score):
+    if fraud_score > 70:
+        user.trust_score -= 10
+    elif fraud_score < 30:
+        user.trust_score += 5
 
-    if user.is_verified:
-        score += 20
-
-    if user.successful_transactions > 5:
-        score += 20
-
-    if user.disputes > 0:
-        score -= 30
-
-    return max(0, min(score, 100))
+    user.trust_score = max(0, min(user.trust_score, 100))
