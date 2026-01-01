f5b6210dfc979cdfd7301c34fa7a47b610824433
PorcupineAI <okerekevip@gmail.com>
2026-01-02 00:36:51 +0100

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    ENV: str = "production"
+    DATABASE_URL: str = Field(..., env="DATABASE_URL")
 
     class Config:
         env_file = ".env"
