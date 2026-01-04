a1d45ea417097a14ce4ab537ee177e47e2034145
PorcupineAI <okerekevip@gmail.com>
2026-01-04 02:08:27 +0100

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,16 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: str = Field(..., env="DATABASE_URL")
+
+    SECRET_KEY: str = Field(..., env="SECRET_KEY")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
         env_file = ".env"
+        extra = "ignore"
+
 
 settings = Settings()
